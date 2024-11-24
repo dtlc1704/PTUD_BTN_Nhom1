1389989b32d749816ae347bd948f7c154bb2dcb3
dtlc1704 <dtlc1704@gmail.com>
2024-11-24 11:39:59 +0700

    Thêm lệnh kết nối db

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root"; // hoặc tên người dùng của bạn
-$password = ""; // mật khẩu nếu có
+$username = "root";
+$password = "";
 $dbname = "cv_database";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Kết nối thất bại: " . $conn->connect_error);
 }
+echo "Kết nối thành công!";
 ?>
