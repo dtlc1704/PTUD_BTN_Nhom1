cab89193db5f1774c94afd806d99138e147f1c11
dtlc1704 <dtlc1704@gmail.com>
2024-11-26 08:02:31 +0700

    Sửa Connet

--- db.php
+++ db.php
@@ -9,5 +9,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
-echo "Kết nối thành công!";
+echo "";
 ?>
